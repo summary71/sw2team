34d1adc67d7343444a4b81e1b32e8842b7a50746
summary71 <summary71@users.noreply.github.com>
2017-09-06 14:34:57 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,4 +1,4 @@
-Firstname = input('enter first name: ')
-Lastname = input('enter last name: ')
-Age = input('enter age: ')
+Firstname = input('enter first name: ') # first name input
+Lastname = input('enter last name: ') # last name input
+Age = input('enter age: ') # age input
 print (Firstname + ' ' + Lastname, int(Age) + 1)
